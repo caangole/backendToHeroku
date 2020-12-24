80dc2ed6c060c3c290b9ecaf66a9fa87879d6ddc
caangole <caangole@gmail.com>
2020-12-24 06:59:22 -0500

    añadí restricciones a los métodos del usuario

--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -6,11 +6,11 @@ const usuarioController = require('../../controllers/UsuarioController.js');
 const auth = require('../../middlewares/auth');
 
 //rutas con restricción de acceso
-router.post('/register',  usuarioController.register);
-router.put('/update', usuarioController.update);
+router.post('/register', auth.verificarAdministrador, usuarioController.register);
+router.put('/update', auth.verificarAdministrador, usuarioController.update);
 router.get('/list', usuarioController.listar);
-router.put('/activate', usuarioController.activate);
-router.put('/deactivate', usuarioController.deactivate);
+router.put('/activate', auth.verificarAdministrador, usuarioController.activate);
+router.put('/deactivate', auth.verificarAdministrador, usuarioController.deactivate);
 
 //ruta login sin restricción de acceso
 router.post('/login', usuarioController.login);
